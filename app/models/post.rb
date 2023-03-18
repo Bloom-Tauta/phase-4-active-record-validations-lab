ea349232b0839c4fe2c5796cacb6c8e565707893
Bloom <bloomtauta@gmail.com>
2023-03-18 21:45:01 +0300

    Completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,8 @@ class Post < ApplicationRecord
     validates :title, presence: true
     validates :content, length: {minimum: 250}
     validates :summary, length: {maximum: 250}
-    # validates :category, inclusion: {:in, message: "Fiction"} 
+    # validates :category, inclusion: {:in, message: "Fiction"}
+    validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
     validate :title_is_clickbait
 
     def title_is_clickbait
